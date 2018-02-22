6a6bfbb77f81d11b4958e55501a34dffb4fd0e07
Yao Yao <yaoy.cs@gmail.com>
2018-02-21 16:15:37 -0800

    added a test case to ensure that the same random_state leads to the same fold partitioning

--- tests/cross_validation_test.py
+++ tests/cross_validation_test.py
@@ -1,5 +1,7 @@
 import unittest
 import pandas as pd
+import numpy as np
+from numpy.testing import assert_array_equal
 from locus_sampling.cross_validation import BalancedGroupKFold
 
 
@@ -25,6 +27,36 @@ class CrossValidationTestCase(unittest.TestCase):
 
             intx_groups = set(train_groups).intersection(set(test_groups))
             self.assertEqual(intx_groups, set())
+
+    def test_random_state(self):
+        group_id_file = "group_id_osu18.tsv"
+        group_id_dfm = pd.read_table(group_id_file, header=0).set_index("name")
+
+        feat_file = "feature_matrix_osu18_2.tsv"
+        label_dfm = pd.read_table(feat_file, header=0).loc[:, ["name", "label"]].set_index("name")
+
+        dfm = group_id_dfm.assign(label=label_dfm["label"])
+        dfm = dfm.reset_index(level="name")
+
+        # CV with the same random_state should yield the same partition
+        cv1 = BalancedGroupKFold(n_splits=10, slop_allowed=0.5, random_state=1337)
+        cv2 = BalancedGroupKFold(n_splits=10, slop_allowed=0.5, random_state=1337)
+        for (train_index_1, test_index_1), (train_index_2, test_index_2) \
+                in zip(cv1.split(dfm, dfm["label"], dfm["group_id"]), cv2.split(dfm, dfm["label"], dfm["group_id"])):
+            assert_array_equal(train_index_1, train_index_2)
+            assert_array_equal(test_index_1, test_index_2)
+
+        # CV with different random_states should yield different partition
+        cv1 = BalancedGroupKFold(n_splits=10, slop_allowed=0.5, random_state=1337)
+        cv2 = BalancedGroupKFold(n_splits=10, slop_allowed=0.5, random_state=1339)
+        for (train_index_1, test_index_1), (train_index_2, test_index_2) \
+                in zip(cv1.split(dfm, dfm["label"], dfm["group_id"]), cv2.split(dfm, dfm["label"], dfm["group_id"])):
+            if len(train_index_1) == len(train_index_2):
+                self.assertTrue(any(np.not_equal(train_index_1, train_index_2)))
+            if len(test_index_1) == len(test_index_2):
+                self.assertTrue(any(np.not_equal(test_index_1, test_index_2)))
+
+            # if index lengths are different, the partition cannot be the same
 
 
 if __name__ == '__main__':
